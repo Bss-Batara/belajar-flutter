39747570b401d245faec8d72ab1f011f465f5469
Batara <bssbatara@gmail.com>
2026-01-06 16:41:49 +0700

    belajar flutter

---
+++ bin/btr_satu.dart
@@ -0,0 +1,18 @@
+import 'package:btr_satu/btr_satu.dart' as btr_satu;
+
+void main(List<String> arguments) {
+  var nama;
+  //nama = "batara";
+  //var angka = "174";
+  //var angka2 = "8.7";
+  //perbandingan = true;
+
+  // var namadepan = 'Batara';
+  // var namabelakang = 'Sarjana Teknik';
+  // var namalengkap = "$namadepan $namabelakang berpenghasilan \$1juta per tahunnya";
+  // print(namalengkap);
+
+  int angka = 10%4;
+  //print(angka++);
+  print(angka);
+}
---
+++ test/btr_satu_test.dart
@@ -0,0 +1,8 @@
+import 'package:btr_satu/btr_satu.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
